4d83ead61751fe0766d49ee859990817084e6455
Sigee <sigee15@gmail.com>
2023-07-21 19:32:19 +0200

    Add first test case
    (cherry picked from commit 0f0cc1d2f7ff1015078812811562b3d2673982cd)

--- src/test/java/mutation_testing/CalculatorTest.java
+++ src/test/java/mutation_testing/CalculatorTest.java
@@ -1,4 +1,20 @@
 package mutation_testing;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd001() {
+        Assertions.assertEquals(0, calculator.add(0, 0));
+    }
 }
